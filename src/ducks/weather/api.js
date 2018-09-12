b547bc4c06a00acdd29f4f5132a3cf158a50d91f
Igor Antonov <igorr1496@gmail.com>
2018-09-12 19:57:14 +0200

    fix(api): change http to https

--- src/ducks/weather/api.js
+++ src/ducks/weather/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const weatherAPI = {
-  get: () => axios.get('http://api.openweathermap.org/data/2.5/forecast?id=554234&units=metric&lang=ru&APPID=3232b4d4a41e715204b4e51216fc82f5')
+  get: () => axios.get('https://api.openweathermap.org/data/2.5/forecast?id=554234&units=metric&lang=ru&APPID=3232b4d4a41e715204b4e51216fc82f5')
 };
